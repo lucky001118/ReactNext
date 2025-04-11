24f0b9a892798ab865766e2fffef712428d57a2b
lucky001118 <manikpurilucky218@gmail.com>
2025-04-11 21:29:44 +0530

    update credentials

---
+++ src/constants/index.jsx
@@ -0,0 +1,36 @@
+export const NAVIGATION_LINKS = [
+    { label: "Projects", href: "#projects" },
+    { label: "Bio", href: "#bio" },
+    { label: "Skills", href: "#skills" },
+    { label: "Work Experience", href: "#work" },
+    { label: "Education", href: "#education" },
+    { label: "activity", href: "#activity" },
+    { label: "Contact", href: "#contact" },
+  ];
+
+  export const SOCIAL_MEDIA_LINKS = [
+    {
+      href: "https://www.facebook.com/lucly.manikpuri",
+      icon: <FaFacebook fontSize={25} className="hover:opacity-80" />,
+    },
+    {
+      href: "https://x.com/",
+      icon: <FaDiscord fontSize={25} className="hover:opacity-80" />,
+    },
+    {
+      href: "https://www.instagram.com/lucky_manikpuri_18",
+      icon: <FaInstagram fontSize={25} className="hover:opacity-80" />,
+    },
+    {
+      href: "https://x.com/",
+      icon: <FaXTwitter fontSize={25} className="hover:opacity-80" />,
+    },
+    {
+      href: "https://github.com/lucky001118",
+      icon: <FaGithub fontSize={25} className="hover:opacity-80" />,
+    },
+    {
+      href: "https://www.linkedin.com/in/lucky-manikpuri-49804927b/",
+      icon: <FaLinkedin fontSize={25} className="hover:opacity-80" />,
+    },
+  ];
